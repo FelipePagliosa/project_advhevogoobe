78ef9f2427b7188e65a433dba764c81f665560fd
Felipe Pagliosa <felipe.coutinho@al.infnet.edu.br>
2020-02-16 00:16:18 -0300

    bimbom

---
+++ app/src/main/java/br/project_advhevogoober_final/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package br.project_advhevogoober_final
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 5000 // 5 sec
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        changeActivity()
+    }
+
+    private fun changeActivity(){
+        Handler().postDelayed ({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
