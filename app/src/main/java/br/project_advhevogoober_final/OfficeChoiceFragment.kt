419206e4ff9e2402fb2ccdf82b8f42bdc169c912
workjohn123 <contitadowow123@gmail.com>
2020-02-21 23:06:47 -0300

    Adicionadas mudanças pro OfficeChoiceFragment

--- app/src/main/java/br/project_advhevogoober_final/OfficeChoiceFragment.kt
+++ app/src/main/java/br/project_advhevogoober_final/OfficeChoiceFragment.kt
@@ -1,12 +1,15 @@
 package br.project_advhevogoober_final
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_office_choice.view.*
 
 class OfficeChoiceFragment:Fragment() {
 
@@ -22,9 +25,25 @@ class OfficeChoiceFragment:Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        Log.d(TAG,"onCreateView")
-        val view: View = inflater!!.inflate(R.layout.fragment_office_choice, container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(TAG, "onCreateView")
+        container?.removeAllViews()// fix milagroso
+        val view: View = inflater!!.inflate(R.layout.fragment_lawyer_choice, container, false)
+        view.btnSalvar.setOnClickListener {
+            if (view.office_name.text.toString() != "" &&
+                view.office_phone.text.toString() != "" &&
+                view.office_business_id.text.toString() != ""
+            ) {
+                var intent = Intent(activity, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(activity, "Preencha todos os campos!!", Toast.LENGTH_LONG).show()
+            }
+        }
         return view
     }
 }
