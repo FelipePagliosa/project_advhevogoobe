86e6c056708394e5731e5adf6b6a1b844da9fe0d
Carlos <carlos.marra@al.infnet.edu.br>
2020-03-04 04:58:05 -0300

    Recycler Home

--- app/src/main/java/br/project_advhevogoober_final/HomeFragment.kt
+++ app/src/main/java/br/project_advhevogoober_final/HomeFragment.kt
@@ -6,11 +6,24 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toast.makeText
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.project_advhevogoober_final.Model.Offer
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class HomeFragment:Fragment() {
 
     val TAG ="HomeFragment"
+    private var offers = mutableListOf<Offer>(Offer("teste1", "teste2",33.25, "teste2","teste1", "teste2","teste1", "teste2"))
+    private var adapter = OfferRecycleAdapter(offers, this::onPostItemClick)
+
+    private fun onPostItemClick(offer: Offer) {
+        makeText(activity, "ok!", Toast.LENGTH_LONG).show()
+    }
 
     override fun onAttach(context: Context) {
         Log.d(TAG,"onAttach")
@@ -25,6 +38,8 @@ class HomeFragment:Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Log.d(TAG,"onCreateView")
         val view: View =inflater!!.inflate(R.layout.fragment_home,container,false)
+        view.recycler_view_home.adapter = adapter
+        view.recycler_view_home.layoutManager = LinearLayoutManager(activity)
         return view
     }
 }
