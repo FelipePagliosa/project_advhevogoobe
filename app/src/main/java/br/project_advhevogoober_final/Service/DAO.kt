b2ded5461877d41e130c44572acdcb173f94bfa7
workjohn123 <contitadowow123@gmail.com>
2020-03-20 00:28:03 -0300

    Muda o tipo de parâmetro no DAO de Path para Query

--- app/src/main/java/br/project_advhevogoober_final/Service/DAO.kt
+++ app/src/main/java/br/project_advhevogoober_final/Service/DAO.kt
@@ -4,15 +4,16 @@ import br.project_advhevogoober_final.Model.APIResultsObject
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface DAO {
 
     @GET("/v1/address?key={key}&street={street}&city={city}&state={state}&postalCode={postalCode}")
     fun show(
-        @Path("key") key : String,
-        @Path("street") street : String,
-        @Path("city") city : String,
-        @Path("state") state : String,
-        @Path("postalCode") postalCode : String
+        @Query("key") key : String,
+        @Query("street") street : String,
+        @Query("city") city : String,
+        @Query("state") state : String,
+        @Query("postalCode") postalCode : String
     ) : Call<APIResultsObject>
 }
