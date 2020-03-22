041bebc861def066abc03afb1f5c2cbc1abe7a46
workjohn123 <contitadowow123@gmail.com>
2020-03-22 07:13:49 -0300

    Deixa os campos do Config nuláveis

--- app/src/main/java/br/project_advhevogoober_final/Model/Config.kt
+++ app/src/main/java/br/project_advhevogoober_final/Model/Config.kt
@@ -1,4 +1,4 @@
 package br.project_advhevogoober_final.Model
 
-class Config(val range:Double, val jurisdictions:List<Boolean>) {
+class Config(val range:Double? = null, val jurisdictions:List<Boolean>? = null) {
 }
