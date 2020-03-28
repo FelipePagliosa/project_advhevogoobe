0c1918ef0e690013c10e8786d0980b386f8d0ed9
workjohn123 <contitadowow123@gmail.com>
2020-03-28 04:31:23 -0300

    Cria a lógica de observação do ViewModel para a mudança no texto do slider e extrai a função de criar um Config

--- app/src/main/java/br/project_advhevogoober_final/ConfigFragment.kt
+++ app/src/main/java/br/project_advhevogoober_final/ConfigFragment.kt
@@ -6,21 +6,23 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import br.project_advhevogoober_final.Model.Config
+import br.project_advhevogoober_final.Model.ViewModel.ConfigViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.SetOptions
 import kotlinx.android.synthetic.main.fragment_config.*
 import kotlinx.android.synthetic.main.fragment_config.view.*
-import kotlinx.android.synthetic.main.fragment_home.*
-import org.imperiumlabs.geofirestore.extension.getLocation
 
 class ConfigFragment:Fragment() {
 
     val TAG ="ConfigFragment"
     val user = FirebaseAuth.getInstance().currentUser!!
     val db = FirebaseFirestore.getInstance()
+    private lateinit var sliderViewModel : ConfigViewModel
 
     override fun onAttach(context: Context) {
         Log.d(TAG,"onAttach")
@@ -34,57 +36,78 @@ class ConfigFragment:Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Log.d(TAG,"onCreateView")
-        val view: View =inflater!!.inflate(R.layout.fragment_config, container,false)
+        val view: View = inflater.inflate(R.layout.fragment_config, container,false)
+
+        sliderViewModel = ViewModelProviders.of(this.activity!!).get(ConfigViewModel::class.java)
 
         view.save_configs.setOnClickListener {
             saveConfig()
         }
 
+        setSliderChangeListener()
+
+        setSliderTextValue()
+
         return view
     }
 
-    fun saveConfig() {
+    private fun setSliderChangeListener() {
+        mapsRange.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                sliderViewModel.sliderValue.value = progress.toDouble()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+    }
+
+    private fun setSliderTextValue() {
+        sliderViewModel.sliderValue.observe(this.activity!!, Observer {
+            if (it != null) {
+                rangeNumber.text = it.toString()
+            }
+        })
+    }
+
+    private fun saveConfig() {
         db.collection("lawyers").document(user.uid).get().addOnSuccessListener {
             if (it.exists()) {
-                val range:Double = (mapsRange.progress / 10).toDouble()
-                val jurisdictions = listOf(
-                    jurisdiction1.isChecked,
-                    jurisdiction2.isChecked,
-                    jurisdiction3.isChecked,
-                    jurisdiction4.isChecked,
-                    jurisdiction5.isChecked,
-                    jurisdiction6.isChecked
-                )
-                val config = Config(range, jurisdictions)
+                val config = setConfig()
                 db.collection("lawyers").document(user.uid).update("config", config)
-                changeFragment()
+                changeFragment(HomeFragment())
             }
         }.addOnFailureListener{
             Log.i("LAWYERS_RETRIEVE_ERROR", "Erro: $it")
         }
         db.collection("offices").document(user.uid).get().addOnSuccessListener {
             if (it.exists()){
-                val range:Double = (mapsRange.progress / 10).toDouble()
-                val jurisdictions = listOf(
-                    jurisdiction1.isChecked,
-                    jurisdiction2.isChecked,
-                    jurisdiction3.isChecked,
-                    jurisdiction4.isChecked,
-                    jurisdiction5.isChecked,
-                    jurisdiction6.isChecked
-                )
-                val config = Config(range, jurisdictions)
+                val config = setConfig()
                 db.collection("offices").document(user.uid).update("config", config)
-                changeFragment()
+                changeFragment(HomeFragment())
             }
         }.addOnFailureListener{
             Log.i("OFFICES_RETRIEVE_ERROR", "Erro: $it")
         }
     }
 
-    fun changeFragment() {
+    private fun setConfig(): Config {
+        val range: Double = (mapsRange.progress / 10).toDouble()
+        val jurisdictions = listOf(
+            jurisdiction1.isChecked,
+            jurisdiction2.isChecked,
+            jurisdiction3.isChecked,
+            jurisdiction4.isChecked,
+            jurisdiction5.isChecked,
+            jurisdiction6.isChecked
+        )
+        return Config(range, jurisdictions)
+    }
+
+    private fun changeFragment(fragment : Fragment) {
         val transaction = fragmentManager?.beginTransaction()
-        val fragment = HomeFragment()
         transaction?.replace(R.id.nav_host_fragment, fragment)
         transaction?.addToBackStack(null)
         transaction?.commit()
