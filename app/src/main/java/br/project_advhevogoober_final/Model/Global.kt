decd9ed0694b99673709a707b90a958d631e7a83
Carlos <carlos.marra@al.infnet.edu.br>
2020-03-30 03:59:58 -0300

    cria uma variavel global

--- app/src/main/java/br/project_advhevogoober_final/Model/Global.kt
+++ app/src/main/java/br/project_advhevogoober_final/Model/Global.kt
@@ -3,8 +3,17 @@ package br.project_advhevogoober_final.Model
 import android.app.Application
 
 
-class Global: Application() {
+class Global {
 
+    companion object{
+        private var instance:Global? = null
+        fun getGlobalInstance():Global{
+            if (instance == null){
+                instance = Global()
+            }
+            return instance!!
+        }
+    }
     var globalOffer:Offer = Offer()
 
     fun setOffer(globalOffer: Offer){
@@ -14,5 +23,4 @@ class Global: Application() {
     fun getOffer(): Offer {
         return globalOffer
     }
-
 }
